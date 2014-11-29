ca8d82a43e1c3e32c9cd7292a52a673679640eec
Kuldeep Yadav <kuldeep5534@gmail.com>
2014-11-29 07:09:54 +0530

    Comparator logic changed.

--- Anagram Sorting/src/in/kuldeepyadav/google/interview/AnagramComparator.java
+++ Anagram Sorting/src/in/kuldeepyadav/google/interview/AnagramComparator.java
@@ -1,9 +1,8 @@
 package in.kuldeepyadav.google.interview;
 
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * {@link Comparator} for comparing two strings,
@@ -18,82 +17,35 @@ public class AnagramComparator implements Comparator<String>{
 	@Override
 	public int compare(String s1, String s2) {
 		
-		if (areAnagrams(s1, s2)) {
-			return 0;
-		}
+		Character[] array1 = ArrayUtils.toObject(s1.toCharArray());
+		Character[] array2 = ArrayUtils.toObject(s2.toCharArray());
 		
-		return compareLexicographically(s1, s2);
-	}
-	
-	/**
-	 * @param s1 first string.
-	 * @param s2 second string.
-	 * @return return true if given two strings are anagram.
-	 */
-	private boolean areAnagrams(String s1, String s2) {
-		
-		if (s1.length() != s2.length()) {
-			return false;
-		}
-		
-		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		
-		for (int i = 0; i < s1.length(); i++) {
-			
-			char stringOneChar = s1.charAt(i);
-			int count = 1;
-			if (map.containsKey(stringOneChar)) {
-				count += map.get(stringOneChar);
-			}
-			map.put(stringOneChar, count);
-			
-			char stringTwoChar = s2.charAt(i);
-			count = -1;
-			if (map.containsKey(stringTwoChar)) {
-				count += map.get(stringTwoChar);
-			}
-			map.put(stringTwoChar, count);
-		}
-		
-		for (Iterator<Character> iterator = map.keySet().iterator(); iterator.hasNext();){
-			Object key = iterator.next();
-			if (map.get(key) != 0) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
+		Sort<Character> sort = new Sort<Character>(new Comparator<Character>() {
 
-	/**
-	 * Compare two strings lexicographically.
-	 * 
-	 * @param s1 first string.
-	 * @param s2 second string.
-	 * @return positive number if first string falls after second,
-	 * 			negative number if first string falls before second,
-	 * 			otherwise zero.
-	 */
-	private int compareLexicographically(String s1, String s2) {
+			@Override
+			public int compare(Character s1, Character s2) {
+				return s1-s2;
+			}
+		});
+		
+		sort.sort(array1);
+		sort.sort(array2);
 		
 		int i = 0;
-		while (i < s1.length() && i < s2.length()) {
-			int difference = s1.charAt(i) - s2.charAt(i);
-			if (difference == 0) {
-				i++;
-				continue;
+		while (i < array1.length && i < array2.length) {
+			int difference = array1[i] - array2[i];
+			if (difference != 0) {
+				return difference;
 			}
-			return difference;
+			i++;
 		}
-
-		if (i < s1.length()) {
+		
+		if (i < array1.length) {
+			return -1;
+		} else if ( i < array2.length) {
 			return 1;
 		}
-		
-		if (i < s2.length()) {
-			return -1;
-		}
-		
 		return 0;
 	}
+
 }
