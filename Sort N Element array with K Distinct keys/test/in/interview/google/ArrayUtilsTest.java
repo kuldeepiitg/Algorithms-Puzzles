b28506c742140a24e9fa5a3d0897698b8e76877b
Kuldeep Yadav <kuldeep5534@gmail.com>
2015-06-25 09:19:01 +0530

    List replaced with arrays.

--- Sort N Element array with K Distinct keys/test/in/interview/google/ArrayUtilsTest.java
+++ Sort N Element array with K Distinct keys/test/in/interview/google/ArrayUtilsTest.java
@@ -24,27 +24,28 @@ public class ArrayUtilsTest {
 		int[] expected = new int[]{0, 2, 2, 4, 5, 5, 6, 7, 7, 8, 9, 10, 10, 10, 11, 12};
 		assertArrayEquals(expected , array);
 		
-		List<Integer> unsortedList = new ArrayList<Integer>();
 		Random random = new Random(System.currentTimeMillis());
-		int listSize = random.nextInt(1000000);
+		int listSize = random.nextInt(200000);
 		int minimum = random.nextInt();
+		
 		System.out.println(listSize);
 		System.out.println(minimum);
+		
+		int[] sortedByNativeMethod = new int[listSize];
 		for (int i = 0; i < listSize; i++) {
-			unsortedList.add(random.nextInt(listSize) + minimum);
+			sortedByNativeMethod[i] = random.nextInt(listSize) + minimum;
 		}
+		int[] sortedByLibrary = sortedByNativeMethod.clone();
 		
-		int[] sortedByLibrary = org.apache.commons.lang3.ArrayUtils.toPrimitive(unsortedList.toArray(new Integer[1]));
 		long startTime = System.nanoTime();
-		Arrays.sort(sortedByLibrary);
-		long endTime = System.nanoTime();
-		System.out.println("time taken by library : " + (endTime - startTime));
-		
-		int[] sortedByNativeMethod = org.apache.commons.lang3.ArrayUtils.toPrimitive(unsortedList.toArray(new Integer[1]));
-		startTime = System.nanoTime();
 		ArrayUtils.sort(sortedByNativeMethod);
-		endTime = System.nanoTime();
+		long endTime = System.nanoTime();
 		System.out.println("time taken by native  : " + (endTime - startTime));
+		
+		startTime = System.nanoTime();
+		Arrays.sort(sortedByLibrary);
+		endTime = System.nanoTime();
+		System.out.println("time taken by library : " + (endTime - startTime));
 		
 		assertArrayEquals(sortedByLibrary, sortedByNativeMethod);
 	}
